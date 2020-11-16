92ce4271152c5929dba18320ac260be170ee7a78
Alexis Agostini <alexisagos@gmail.com>
2020-11-16 16:39:03 +0100

    Change logo height to 100px

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         <v-container>
           <img
             :src="logoSrcUrl"
-            height="50px">
+            height="100px">
           <router-view/>
         </v-container>
       </v-content>
