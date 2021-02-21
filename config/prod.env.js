f241164eb08adc7b25e5ce642ddc0208240dd2b2
Alexis Agostini <alexisagos@gmail.com>
2021-02-21 21:15:56 +0100

    Update KINO_RESEARCH_PRYV_API_PROD

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,5 +5,5 @@ module.exports = {
   KINO_RESEARCH_API_DEV: '"https://kino-research-uhcjixolra-ew.a.run.app"',
   KINO_RESEARCH_API_PROD: '"https://kino-research-wjiyq4pn3q-ew.a.run.app"',
   KINO_RESEARCH_PRYV_API_DEV: '"https://kino-research-pryv-uhcjixolra-ew.a.run.app"',
-  KINO_RESEARCH_PRYV_API_PROD: '"https://kino-core-pryv-wjiyq4pn3q-ew.a.run.app"'
+  KINO_RESEARCH_PRYV_API_PROD: '"https://kino-research-pryv-wjiyq4pn3q-ew.a.run.app"'
 }
